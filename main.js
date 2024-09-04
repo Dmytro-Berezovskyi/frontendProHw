a3b049e35d05b9f8cd39de92ee7f91aeeeec3ed9
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-09-04 20:51:22 +0200

    hw1 commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let numberType = 1;
+let stringType = "Hello";
+let booleanType = true;
+let nullType = null;
+let undefinedType;
+let idType = Symbol("id");
+
+console.log(typeof(numberType), typeof(stringType), typeof(booleanType), typeof(nullType), typeof(undefinedType), typeof(idType));
