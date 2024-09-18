e4520a4bd293d210ed9e887b9304d9ffe40fc4e7
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-09-18 18:32:03 +0200

    change l6hw2

---
+++ main.js
@@ -0,0 +1,14 @@
+let arr = [5, 8, true, "fewf", 65, "fger", 3];
+
+function arithmeticMean () {
+    let arrNumber = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i === typeof Number) {
+            arrNumber.push(i);
+        };
+    };
+    return arrNumber;
+};
+
+arithmeticMean(arr);
+console.log(arrNumber);
