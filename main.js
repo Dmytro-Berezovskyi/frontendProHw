3f9ee56cce8586519680403b5a5053c7ffb71641
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-09-18 19:31:22 +0200

    commit l6hw2

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ let arr = [5, 8, true, "fewf", 65, "fger", 3];
 function arithmeticMean () {
     let arrNumber = [];
     for (let i = 0; i < arr.length; i++) {
-        if (i === typeof Number) {
-            arrNumber.push(i);
+        if (typeof arr[i] === 'number') {
+            arrNumber.push(arr[i]);
         };
     };
-    return arrNumber;
+    let sum = 0;
+    for (let i = 0; i < arrNumber.length; i++) {
+        sum += arrNumber[i];
+    };
+    let mean = sum / arrNumber.length;
+    console.log(arrNumber);
+    console.log(sum);
+    console.log(mean);
+    return mean;
 };
 
 arithmeticMean(arr);
-console.log(arrNumber);
