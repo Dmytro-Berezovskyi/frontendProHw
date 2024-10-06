cef13bc93668f6877eeea36f349e43b69cd9bc27
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-06 17:47:22 +0200

    l11hw3

---
+++ main.js
@@ -0,0 +1,8 @@
+const img = document.querySelector('.random_img');
+
+let randomNum = Math.floor((Math.random() * (9 - 1 + 1) + 1));
+img.src = `./img/${randomNum}.jpg`;
+
+
+console.log(randomNum);
+
