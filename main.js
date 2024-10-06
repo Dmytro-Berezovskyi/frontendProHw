b31adfcd81e400fd22ae8558c4e306ed5373ef8c
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-06 14:45:36 +0200

    commit l11hw1

---
+++ main.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const table = document.createElement('table');
+
+table.classList.add('my_table');
+body.appendChild(table);
+
+for (let i = 0; i < 10; i++) {
+    table.appendChild(document.createElement('tr'));
+};
+
+const trs = document.querySelectorAll('tr');
+
+for (let i = 0; i < 10; i++) {
+    trs.forEach(tr => tr.appendChild(document.createElement('td')));
+};
+
+for (let i = 0; i < table.children[0].children.length; i++) {
+
+
+}
+
+console.log();
