3ff05591ecb7775837635def59d265a191497a5f
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-09 11:40:40 +0200

    das

---
+++ main.js
@@ -0,0 +1,8 @@
+const first_btn = document.getElementsByClassName('first_btn');
+const second_btn = document.getElementsByClassName('second-btn');
+
+console.log(first_btn);
+
+document.addEventListener('click', () => {
+    let link = prompt("Enter your link");
+})
