3e58b612aa8e8c90baf99576840b76602447a896
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-13 22:21:07 +0200

    commit l12hw2

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 document.querySelector('#container').addEventListener('click', (event) => {
-    console.log(event.target.value);
-    alert(`Клікнуто на кнопці: ${event.target.value}`);
-})
+    alert(`Клікнуто на кнопці: ${event.target.textContent}`);
+});
