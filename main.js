68add8fa3e5eda38462b1b301eadaa6d625693fd
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-13 22:03:14 +0200

    commit l12hw1

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const first_btn = document.querySelector('.first_btn');
-const second_btn = document.querySelector('.second-btn');
-
-console.log(first_btn);
+const second_btn = document.querySelector('.second_btn');
 
 let link = "";
+
 first_btn.addEventListener('click', () => {
     link = prompt("Enter your link");
 });
-second_btn.addEventListener('click', () => {
 
-})
+second_btn.addEventListener('click', () => {
+    location.href = "https://"+link;
+});
