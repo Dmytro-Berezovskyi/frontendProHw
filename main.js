af7f58133b29bb867d37b05ea32206d1c041e12f
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-14 21:25:43 +0200

    l12hw3

--- main.js
+++ main.js
@@ -3,11 +3,19 @@ const inputTodo = document.querySelector('#input');
 const btnAdd = document.querySelector('#btn_add_todo');
 
 btnAdd.addEventListener('click', () => {
-    const newTodo = document.createElement("span");
-    newTodo.textContent = inputTodo.value;
+    const newTodo = document.createElement("div");
+    const todoText = document.createElement("span")
+    const deleteBtn = document.createElement("button");
+    todoText.textContent = inputTodo.value;
+    deleteBtn.textContent = "Delete";
     newTodo.classList.add('todo');
-
+    deleteBtn.classList.add('btn_delete');
     todoList.appendChild(newTodo);
+    newTodo.appendChild(todoText);
+    newTodo.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', () => {
+        newTodo.removeChild()
+    })
 
     inputTodo.value = "";
 });
