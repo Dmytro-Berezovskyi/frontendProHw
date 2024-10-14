a1213a905465caf8a2f2dcf22cf894211e774125
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-14 21:47:43 +0200

    l12hw1

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const first_btn = document.querySelector('.first_btn');
-const second_btn = document.querySelector('.second_btn');
-
 let link = "";
 
-first_btn.addEventListener('click', () => {
+document.querySelector('.first_btn').addEventListener('click', () => {
     link = prompt("Enter your link (for example: google.com)");
 });
 
-second_btn.addEventListener('click', () => {
+document.querySelector('.second_btn').addEventListener('click', () => {
     location.href = "https://"+link;
 });
