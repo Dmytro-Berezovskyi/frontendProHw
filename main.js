13deb67f33406fbddf9c4285bee101df3d64f0b5
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-15 21:03:06 +0200

    l13hw1

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('#form');
+console.log(form);
+const userInfo = {};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    userInfo.name = form.querySelector('#name').value;
+    userInfo.email = form.querySelector('#email').value;
+    userInfo.phone = form.querySelector('#phone').value;
+    userInfo.message = form.querySelector('#message').value;
+
+    console.log("User Date:", userInfo);
+})
