c06439ba5457d74babefceec5c6546c834b8bdea
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-15 22:25:40 +0200

    l13hw1

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 const form = document.querySelector('#form');
-console.log(form);
 const userInfo = {};
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const phoneRegex = /^\+380[3-9][0-9]{8}$/;
 
 function validateForm() {
     let isValid = true;
 
     if (!userInfo.name) {
         document.querySelector('#errName').innerHTML = "Імʼя - це обовʼязкове поле";
+        isValid = false;
+    };
+
+    if (userInfo.message.length < 5) {
+        document.querySelector('#errMessage').innerHTML = "Повідомлення має містити не менше 5 символів"
+        isValid = false;
+    };
+
+    if (!emailRegex.test(userInfo.email)) {
+        document.querySelector('#errEmail').innerHTML = "Email повинен містити @ та .";
+        isValid = false;
+    };
+
+    if (!phoneRegex.test(userInfo.phone)) {
+        document.querySelector('#errPhone').innerHTML = "Номер телефону має починатись з +380"
+        isValid = false;
     };
 
     return isValid;
@@ -14,10 +31,14 @@ function validateForm() {
 
 function clearErr() {
     document.querySelector('#errName').innerHTML = "";
-}
+    document.querySelector('#errMessage').innerHTML = "";
+    document.querySelector('#errEmail').innerHTML = "";
+    document.querySelector('#phone').innerHTML = "";
+};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    clearErr();
 
     userInfo.name = form.querySelector('#name').value;
     userInfo.email = form.querySelector('#email').value;
@@ -29,5 +50,4 @@ form.addEventListener('submit', (e) => {
     } else {
         console.log("User date isn't valid!");
     }
-
 });
