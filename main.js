8c53e64171d330f925de0414c66b34b61a646f14
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-21 21:07:43 +0200

    d

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const todoList = document.querySelector('#todo_list');
 const inputTodo = document.querySelector('#input');
 
+let todoItems = JSON.parse(localStorage.getItem('todoItems')) || [];
+
+console.log(todoItems);
+function saveTodoToLocalStorage() {
+    localStorage.setItem('todoItem', JSON.stringify(newTodo));
+}
+
 document.querySelector('#btn_add_todo').addEventListener('click', () => {
     const newTodo = document.createElement("div");
+    const todoCheckbox = document.createElement("input");
     const todoText = document.createElement("span");
     const deleteBtn = document.createElement("button");
 
@@ -11,8 +19,11 @@ document.querySelector('#btn_add_todo').addEventListener('click', () => {
 
     newTodo.classList.add('todo');
     deleteBtn.classList.add('btn_delete');
+    todoCheckbox.name = "checkbox";
+    todoCheckbox.type = "checkbox";
 
     todoList.appendChild(newTodo);
+    newTodo.appendChild(todoCheckbox);
     newTodo.appendChild(todoText);
     newTodo.appendChild(deleteBtn);
 
@@ -20,5 +31,10 @@ document.querySelector('#btn_add_todo').addEventListener('click', () => {
         todoList.removeChild(newTodo)
     });
 
+    const objNewTodo = {newTodo};
+    console.log(objNewTodo);
+    todoItems.push(objNewTodo);
+    console.log(newTodo);
+    saveTodoToLocalStorage();
     inputTodo.value = "";
 });
