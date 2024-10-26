dc459b5543fc208aaeed43da0310def5eafe2f6a
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-26 16:35:04 +0200

    l17hw1

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 class Calculator {
-    
+    add (firstValue, secondValue) {
+        return firstValue + secondValue;
+    }
+    subtract (firstValue, secondValue) {
+        return firstValue - secondValue;
+    }
+    multiply (firstValue, secondValue) {
+        return firstValue * secondValue;
+    }
+    divide (firstValue, secondValue) {
+        return firstValue / secondValue;
+    }
 }
+
+const calc = new Calculator();
+
+console.log(calc.add(5, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6));
+console.log(calc.divide(8, 2));
