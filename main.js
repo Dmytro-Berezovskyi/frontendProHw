cb3ed30cd3f4af88da33f94a567e4f3358c2c12e
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-10-30 21:43:15 +0100

    1

---
+++ main.js
@@ -0,0 +1,44 @@
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+
+
+let startTime = "01:45";
+
+let givenMinutes = startTime.slice(0, 2);
+let givenSeconds = startTime.slice(3, 5);
+minutes.innerHTML = givenMinutes;
+seconds.innerHTML = givenSeconds;
+
+let secondsIntervalId = null;
+let minutesIntervalId = null;
+
+document.querySelector("#start").addEventListener("click", () => {
+    secondsIntervalId = setInterval(() => {
+        if (givenSeconds !== "00") {
+            givenSeconds--;
+        }
+        seconds.innerHTML = givenSeconds;
+        if (givenSeconds < 10 && givenSeconds !== "00") {
+            seconds.innerHTML = `0${givenSeconds}`;
+        }
+        if (givenSeconds === 0) {
+            clearInterval(secondsIntervalId);
+        }
+    },1000)
+
+    minutesIntervalId = setInterval(() => {
+        givenMinutes--;
+        minutes.innerHTML = givenMinutes;
+        if (givenMinutes < 10) {
+            minutes.innerHTML = `0${givenMinutes}`;
+        }
+        if (givenMinutes === 0) {
+            clearInterval(minutesIntervalId);
+        }
+    }, 60000)
+});
+
+document.querySelector("#stop").addEventListener("click", () => {
+    clearInterval(secondsIntervalId);
+    clearInterval(minutesIntervalId);
+});
