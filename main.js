7c7746820e9d90041ba48cab7624939bfe3b1e1c
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2024-11-09 16:13:33 +0100

    l20hw1 1.3

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const formNewPost = document.querySelector("#form-add_post");
+//Initial set
 const containerPosts = document.querySelector('#container-posts');
 const formAddPost = document.querySelector('#form-add_post');
+const inputTitle = document.querySelector('#input-title');
+const inputBody = document.querySelector("#input-body");
 
-const postContainer = document.createElement("div");
-postContainer.classList.add("post-container");
-containerPosts.insertBefore(postContainer, formAddPost);
+const postContainers = {};
+
 //Function
 const loadPosts = () => {
     fetch('https://jsonplaceholder.typicode.com/posts?_limit=10').then((response) => {
@@ -16,24 +17,61 @@ const loadPosts = () => {
     });
 }
 
-const loadComments = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts/${post.id}/comments?_limit=2').then((response) => {
+const loadComments = (postId) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments?_limit=2`).then((response) => {
         return response.json();
     }).then((data) => {
-        data.forEach((comment) => {addCommentToDom(comment)})
+        data.forEach((comment) => {addCommentToDom(comment, postId)})
     }).catch((error) => {
         console.log(error)
     })
 }
 
-const addCommentToDom = (comment) => {
-    const commentsContainer = document.createElement('div')
+const addPostToDom = (post) => {
+    const postContainer = document.createElement("div");
+    const title = document.createElement("h3");
+    const userPost = document.createElement("span");
+    const line = document.createElement("div");
+    const btnComments = document.createElement("button");
+    const commentsContainer = document.createElement('div');
+
+    postContainer.classList.add("post-container");
+    title.classList.add("title");
+    userPost.classList.add("user-post");
+    line.classList.add("line");
+    btnComments.classList.add("comments");
+    commentsContainer.classList.add("comments-container");
+
+    title.innerHTML = post.title;
+    userPost.innerHTML = post.body;
+    btnComments.innerHTML = "Last comments";
+
+    containerPosts.insertBefore(postContainer, formAddPost);
+    postContainer.appendChild(title);
+    postContainer.appendChild(userPost);
+    postContainer.appendChild(line);
+    postContainer.appendChild(btnComments);
+    postContainer.appendChild(commentsContainer);
+
+    postContainers[post.id] = {
+        postContainer: postContainer,
+        commentsContainer: commentsContainer,
+    };
+
+    loadComments(post.id);
+
+    btnComments.addEventListener('click', () => {
+        commentsContainer.classList.toggle('active');
+    });
+};
+
+const addCommentToDom = (comment, postId) => {
+    const commentsContainer = postContainers[postId].commentsContainer;
     const commentBlock = document.createElement("div");
     const commentName = document.createElement("span");
     const commentEmail = document.createElement("span");
     const commentTxt = document.createElement("span");
 
-    commentsContainer.classList.add("comments-container");
     commentBlock.classList.add("comment-block");
     commentName.classList.add("comment-name");
     commentEmail.classList.add("comment-email");
@@ -43,116 +81,37 @@ const addCommentToDom = (comment) => {
     commentName.innerText = comment.name;
     commentTxt.innerHTML = comment.body;
 
-    postContainer.appendChild(commentsContainer);
     commentsContainer.appendChild(commentBlock);
     commentBlock.appendChild(commentName);
     commentBlock.appendChild(commentEmail);
     commentBlock.appendChild(commentTxt);
 }
 
-const addPostToDom = (post) => {
-    const title = document.createElement("h3");
-    const userPost = document.createElement("span");
-    const line = document.createElement("div");
-    const btnComments = document.createElement("button");
+function statusSuccess() {
+    const postStatus = document.createElement('span');
+    postStatus.classList.add("status-success");
+    postStatus.innerHTML = "Post successfully added";
+    formAddPost.insertBefore(postStatus, inputTitle);
 
-    title.classList.add("title");
-    userPost.classList.add("user-post");
-    line.classList.add("line");
-    btnComments.classList.add("comments");
+    setTimeout(() => {
+        postStatus.remove();
+    }, 3000)
+}
 
-    title.innerHTML = post.title;
-    userPost.innerHTML = post.body;
-    btnComments.innerHTML = "Last comments";
+function statusFailed() {
+    const postStatus = document.createElement('span');
+    postStatus.classList.add("status-failed");
+    postStatus.innerHTML = "Enter the title and content of the post !";
+    formAddPost.insertBefore(postStatus, inputTitle);
 
-    postContainer.appendChild(title);
-    postContainer.appendChild(userPost);
-    postContainer.appendChild(line)
-    postContainer.appendChild(btnComments);
+    setTimeout(() => {
+        postStatus.remove();
+    }, 5000)
+}
 
-    const commentsContainer = document.createElement("div");
-
-    commentsContainer.classList.add("comments-container");
-
-    postContainer.appendChild(commentsContainer);
-};
-
-//function renderPosts(data) {
-//    data.forEach((post) => {
-//        const containerPosts = document.querySelector('#container-posts');
-//        const formAddPost = document.querySelector('#form-add_post');
-//        const postContainer = document.createElement("div");
-//        const title = document.createElement("h3");
-//        const userPost = document.createElement("span");
-//        const line = document.createElement("div");
-//        const btnComments = document.createElement("button");
-//
-//        postContainer.classList.add("post-container");
-//        title.classList.add("title");
-//        userPost.classList.add("user-post");
-//        line.classList.add("line");
-//        btnComments.classList.add("comments");
-//
-//        title.innerHTML = post.title;
-//        userPost.innerHTML = post.body;
-//        btnComments.innerHTML = "Last comments";
-//
-//        containerPosts.insertBefore(postContainer, formAddPost);
-//        postContainer.appendChild(title);
-//        postContainer.appendChild(userPost);
-//        postContainer.appendChild(line)
-            //        postContainer.appendChild(btnComments);
-//
-//        const commentsContainer = document.createElement("div");
-//
-//        commentsContainer.classList.add("comments-container");
-//
-//        postContainer.appendChild(commentsContainer);
-//
-//        fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments?_limit=2`)
-            //            .then(res => res.json()).then(data => {
-//            data.forEach((comment) => {
-//                const commentBlock = document.createElement("div");
-//                const commentName = document.createElement("span");
-//                const commentEmail = document.createElement("span");
-//                const commentTxt = document.createElement("span");
-//
-//                commentBlock.classList.add("comment-block");
-//                commentName.classList.add("comment-name");
-//                commentEmail.classList.add("comment-email");
-//                commentTxt.classList.add("comment-txt");
-//
-//                commentEmail.innerText = comment.email;
-//                commentName.innerText = comment.name;
-//                commentTxt.innerHTML = comment.body;
-//
-//                commentsContainer.appendChild(commentBlock);
-//                commentBlock.appendChild(commentName);
-//                commentBlock.appendChild(commentEmail);
-//                commentBlock.appendChild(commentTxt);
-//            })
-                    //        })
-            //
-            //        btnComments.addEventListener('click', () => {
-//            commentsContainer.classList.toggle('active');
-//        })
-            //    })
-        //}
-////Main
-//fetch('https://jsonplaceholder.typicode.com/posts?_limit=10').then((response) => {
-//    return response.json();
-//}).then((data) => {
-//    console.log(data);
-//    renderPosts(data);
-//});
-
-let newPostData = [];
-
-formNewPost.addEventListener("submit", (e) => {
+//Main
+formAddPost.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    const inputTitle = document.querySelector("#input-title");
-    const inputBody = document.querySelector("#input-body");
 
     const title = inputTitle.value.trim();
     const body = inputBody.value.trim();
@@ -173,17 +132,15 @@ formNewPost.addEventListener("submit", (e) => {
             return response.json();
         }).then((data) => {
             addPostToDom(data)
+
+            statusSuccess();
         }).catch((error) => {console.log(error)});
+    } else {
+        statusFailed();
     }
 
     inputTitle.value = "";
     inputBody.value = "";
 })
 
-btnComments.addEventListener('click', () => {
-    commentsContainer.classList.toggle('active');
-}
-
 loadPosts();
-loadComments();
-console.log(newPostData);
