ce4fcd7f786930eed52cde7dbc71327ee3451e25
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2025-01-03 21:41:29 +0100

    2

--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const data = require('./db.json');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const data = require("./db.json");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/destination', (req, res) => {
+app.get("/destination", (req, res) => {
     res.json(data.destination);
 });
 
-app.get(`/hotels`, (req, res) => {
+app.get("/hotels", (req, res) => {
     res.json(data.hotels);
 });
-app.post('/hotels', (req, res) => {
+app.post("/hotels", (req, res) => {
     const { city } = req.body;
     const hotels = data.hotels.filter((hotel) => hotel.city === city);
+
     res.json(hotels);
 });
 
